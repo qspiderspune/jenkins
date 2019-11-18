f70e87fcf1b1d80f85210a2d506e0fa007e616c4
sandeepini <sandeepkondaiah@gmail.com>
2019-11-18 12:23:05 +0530

    first

---
+++ Jenkins/src/test/java/com/vtiger/test/ValidLogin.java
@@ -0,0 +1,19 @@
+package com.vtiger.test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ValidLogin {
+	@Test
+	public void testCreateUser() {
+		Reporter.log("testCreateUser",true);
+	}
+	@Test
+	public void testEditUser() {
+		Reporter.log("testEditUser",true);
+	}
+	@Test
+	public void testDeleteUser() {
+		Reporter.log("testDeleteUser",true);
+	}
+}
